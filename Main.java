d21e50289b89967880ed0dbc4b9f52d07f3a1a74
Keri Southwood-Smith <keri.southwood@gmail.com>
2022-06-17 23:56:36 -0400

    Stage 1: Completion
    Print title of program on start. Then get command inputs from user.
    Print "Bye!" when command "exit" is entered, all other commands get
    an error message. Blank line inputs (including just white space)
    get a "No input." message.

--- Main.java
+++ Main.java
@@ -1,7 +1,34 @@
 package tracker;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Learning Progress Tracker");
+        processCommands();
+        System.out.println("Bye!");
+    }
+
+    private static void processCommands() {
+        boolean getCommands = true;
+
+        while (getCommands) {
+            String command = getInput();
+            if (command.isBlank()) {
+                System.out.println("No input.");
+                continue;
+            }
+
+            switch (command) {
+                case "exit" -> getCommands = false;
+                default -> System.out.println("Error: unknown command!");
+            }
+        }
+    }
+
+    private static String getInput() {
+        return SCANNER.nextLine().toLowerCase();
     }
 }
